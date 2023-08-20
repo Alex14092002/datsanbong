1a89a8abf31419f7fa02efdc7afe638de27af0d9
PHU TUAN <2051010343tuan@ou.edu.vn>
2023-08-20 19:34:51 +0700

    update

--- js/date.js
+++ js/date.js
@@ -1,56 +1,42 @@
-function generateDateOptions() {
-    const today = new Date();
-    const options = [];
-  
-    for (let i = 0; i < 31; i++) {
-      const date = new Date(today);
-      date.setDate(today.getDate() + i);
-      const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-      options.push(formattedDate);
-    }
-  
-    return options;
+document.addEventListener("DOMContentLoaded", function () {
+  const dateSelect = document.getElementById("dateSelect");
+
+  // Tạo mảng chứa 7 ngày tiếp theo (tính từ ngày hiện tại)
+  const dateOptions = [];
+  for (let i = 0; i < 7; i++) {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + i);
+    const formattedDate = currentDate.toLocaleDateString("vi-VN", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    });
+    dateOptions.push({ value: formattedDate, text: formattedDate });
   }
-  
-  const dateOptions = generateDateOptions();
-  
-  const selectElement = document.createElement('select');
-  selectElement.name = 'selectedDate';
-  
-  for (const optionText of dateOptions) {
-    const optionElement = document.createElement('option');
-    optionElement.value = optionText;
-    optionElement.textContent = optionText;
-    selectElement.appendChild(optionElement);
-  }
-  
-  const viewDayDiv = document.querySelector('.view-day')
 
-  viewDayDiv.innerHTML = `
-    <label for="selectedDate">Xem Ngày</label>
-  `;
-  
-  viewDayDiv.appendChild(selectElement);
+  // Thêm các tùy chọn vào phần tử select
+  dateOptions.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option.value;
+    optionElement.textContent = option.text;
+    dateSelect.appendChild(optionElement);
+  });
+});
 
 
 
-  function generateTimeSlots() {
-    const timeSlots = [];
-    for (let hour = 4; hour < 24; hour++) {
-      for (let minute = 0; minute < 60; minute += 30) {
-        const formattedHour = hour.toString().padStart(2, '0');
-        const formattedMinute = minute.toString().padStart(2, '0');
-        const timeSlot = `${formattedHour}h${formattedMinute}`;
-        timeSlots.push(timeSlot);
-      }
-    }
-    return timeSlots;
-  }
+dateSelect.addEventListener("change", function () {
+  const selectedDateParts = dateSelect.value.split("/"); // Chuyển đổi thành mảng các phần ngày, tháng, năm
+  const selectedDate = new Date(
+    selectedDateParts[2], // Năm
+    selectedDateParts[1] - 1, // Tháng (để trừ đi 1 vì tháng trong Date bắt đầu từ 0)
+    selectedDateParts[0] // Ngày
+  );
   
-  const timeSlots = generateTimeSlots();
-  
-  // // In ra danh sách các khung giờ
-  // timeSlots.forEach(timeSlot => {
-  //   console.log(timeSlot);
-  // });
-  
+  // Tại đây, bạn có thể xử lý hiển thị thông tin đặt sân cho ngày đã chọn.
+  // Bạn có thể sử dụng mã nguồn hiện tại của mình để tạo bảng và cập nhật sân trống.
+
+  console.log("Ngày đã chọn:", selectedDate.toLocaleDateString("vi-VN"));
+});
+
+
