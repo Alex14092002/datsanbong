80a0cf3c79b8efcaf49911667f8dbc7a13e957d0
PHU TUAN <2051010343tuan@ou.edu.vn>
2023-08-20 18:22:17 +0700

    update

--- js/header.js
+++ js/header.js
@@ -3,9 +3,11 @@ const phone = localStorage.getItem('signedInPhone')
 console.log(phone);
 if(phone){
     header.innerHTML += `
-<h5>Web đặt sân cầu lông</h1>
+    <div>
+        <img src="https://media.istockphoto.com/id/525417233/vi/vec-to/logo-c%C3%A2u-l%E1%BA%A1c-b%E1%BB%99-b%C3%B3ng-c%E1%BA%A7u-l%C3%B4ng.jpg?s=170667a&w=0&k=20&c=3SR9wePFWD3euz-YHQLnyWYoJPcdfa0zRITn8EBfoxw=" width="100px"/>
+    </div>
 <div>
-<a class="btn btn-primary" href="">Xin chào ${phone}</a>
+<a class="btn btn-primary" href="../edit_user.html">Xin chào ${phone}</a>
 <button type="button" id="logOut" class="btn btn-danger">Đăng xuất</button>
 </div>
 
