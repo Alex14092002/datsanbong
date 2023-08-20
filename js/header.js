33644cc4d87c493ded96dc4daf2b8615e8630359
PHU TUAN <2051010343tuan@ou.edu.vn>
2023-08-20 18:42:50 +0700

    update

--- js/header.js
+++ js/header.js
@@ -4,7 +4,10 @@ console.log(phone);
 if(phone){
     header.innerHTML += `
     <div>
+        <a href="../index.html">
         <img src="https://media.istockphoto.com/id/525417233/vi/vec-to/logo-c%C3%A2u-l%E1%BA%A1c-b%E1%BB%99-b%C3%B3ng-c%E1%BA%A7u-l%C3%B4ng.jpg?s=170667a&w=0&k=20&c=3SR9wePFWD3euz-YHQLnyWYoJPcdfa0zRITn8EBfoxw=" width="100px"/>
+        <a/>
+       
     </div>
 <div>
 <a class="btn btn-primary" href="../edit_user.html">Xin chào ${phone}</a>
